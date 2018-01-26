ce30956afd80d259752c049ab8916e30fc3449a0
lolilukia <lolilukia@163.com>
2018-01-27 00:29:41 +0800

    v1.1: modify sign success page

--- pages/signSuccess/signSuccess.js
+++ pages/signSuccess/signSuccess.js
@@ -6,11 +6,62 @@ Page({
     signsuText: '报名成功',
     checkText: '签到',
     cancelText: '退报名',
-    backText: '取消'
+    backText: '取消',
+    stunum: '',
+    returnCode: ''
   },
   jumpCheck: function () {
-    wx.redirectTo({
-      url: '../checkIn/checkIn'
+    var that = this;
+    wx.getStorage({
+      key: 'stuNum',
+      success: function (res) {
+        that.setData({
+          stunum: res.data
+        })
+        wx.request({
+          url: 'https://www.jdyx.club/tjyx_backend/web/index.php?r=sign/sign',
+          method: 'POST',
+          header: {
+            'content-type': 'application/x-www-form-urlencoded'
+          },
+          data: {
+            stunum: that.data.stunum,
+          },
+          success: function (result) {
+            that.setData({
+              returnCode: result.data.state
+            });
+            if (that.data.returnCode.indexOf('hasCheck') != -1) {
+              wx.redirectTo({
+                url: '../hasCheck/hasCheck',
+              });
+            }
+            else if (that.data.returnCode.indexOf('noSign') != -1) {
+              wx.redirectTo({
+                url: '../noSign/noSign',
+              });
+            }
+            else if (that.data.returnCode.indexOf('signTimeError') != -1) {
+              wx.redirectTo({
+                url: '../signTimeError/signTimeError',
+              });
+            }
+            else if (that.data.returnCode.indexOf('insufficient') != -1) {
+              wx.redirectTo({
+                url: '../insufficient/insufficient',
+              });
+            }
+            else if (that.data.returnCode.indexOf('success') != -1) {
+              wx.redirectTo({
+                url: '../checkIn/checkIn',
+              });
+            }
+            else {
+              console.log(that.data.returnCode);
+            }
+          }
+        })
+      }
     })
   },
   jumpCancel: function () {
